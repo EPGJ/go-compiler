65aecc08f2762adb6d37bc7b0383ca5da4892184
Elias Junior <eliasepgj@gmail.com>
2022-02-15 08:22:42 -0300

    Adicionando novas regras ao SemanticChecker

--- src/checker/SemanticChecker.java
+++ src/checker/SemanticChecker.java
@@ -227,48 +227,45 @@ public class SemanticChecker extends GoParserBaseVisitor<Type> {
 		System.out.print("\n\n");
 	}
 
-	// Visits the rule var_types: INT
+	// Visita a regra var_types: INT
 	@Override
 	public Type visitIntType(GoParser.IntTypeContext ctx) {
 		this.lastDeclType = Type.INT_TYPE;
 		return Type.NO_TYPE;
 	}
 
-	// Visits the rule var_types: STRING
+	// Visita a regra var_types: STRING
 	@Override
 	public Type visitStringType(GoParser.StringTypeContext ctx) {
 		this.lastDeclType = Type.STR_TYPE;
 		return Type.NO_TYPE;
 	}
 
-	// Visits the rule var_types: BOOL
+	// Visita a regra var_types: BOOL
 	@Override
 	public Type visitBoolType(GoParser.BoolTypeContext ctx) {
 		this.lastDeclType = Type.BOOL_TYPE;
 		return Type.NO_TYPE;
 	}
 
-	// Visits the rule var_types: FLOAT32
+	// Visita a regra var_types: FLOAT32
 	@Override
 	public Type visitFloat32Type(GoParser.Float32TypeContext ctx) {
 		this.lastDeclType = Type.FLOAT32_TYPE;
 		return Type.NO_TYPE;
 	}
 
-	// Visita a regra var_declaration: VAR IDENTIFIER (var_types | var_types? ASSIGN  expression | array_declaration) SEMI?
+	// Visita a regra var_declaration: VAR IDENTIFIER (var_types | var_types? ASSIGN
+	// expression | array_declaration) SEMI?
 	@Override
 	public Type visitVar_declaration(GoParser.Var_declarationContext ctx) {
 		// Checa se a variável é um array ou uma variável normal
 		if (ctx.array_declaration() != null) {
-			// Recursively visits the array_declaration to define the array type
+			// Visita recursivamente array_declaration para definir o tipo do array
 			visit(ctx.array_declaration());
 		} else {
 			boolean hasVarType = ctx.var_types() != null;
 
-			// Defines the var type based on the explicit type declaration or
-			// the given initial expression.
-			// e.g: var x int
-			// var x = 10
 			if (hasVarType) {
 				// Define lastDeclType com a declaração explícita do tipo da variável
 				visit(ctx.var_types());
@@ -280,11 +277,11 @@ public class SemanticChecker extends GoParserBaseVisitor<Type> {
 
 		Token identifierToken = ctx.IDENTIFIER().getSymbol();
 
-		// Checks if the variable was previously declared
+		// Verifica se a variável foi previamente declarada
 		newVar(identifierToken);
 
 		boolean hasAssign = ctx.ASSIGN() != null;
-		// Checks if the identifier type and expression type match
+
 		if (hasAssign) {
 			Type identifierType = checkVar(identifierToken);
 			Type expressionType = visit(ctx.expression());
@@ -294,5 +291,216 @@ public class SemanticChecker extends GoParserBaseVisitor<Type> {
 
 		return Type.NO_TYPE;
 	}
+
+	// Visita a regra declare_assign: IDENTIFIER DECLARE_ASSIGN ( array_init |
+	// expression) SEMI?
+	@Override
+	public Type visitDeclare_assign(GoParser.Declare_assignContext ctx) {
+		Token identifierToken = ctx.IDENTIFIER().getSymbol();
+
+		// Define lastDeclType com base no tipo da expressão ou na inicialização do
+		// array
+		if (ctx.expression() != null)
+			lastDeclType = visit(ctx.expression());
+		if (ctx.array_init() != null) {
+			visit(ctx.array_init());
+
+			// Verifica se o array foi inicializado com a quantidade correta de indices
+			checkArrayInit(identifierToken.getLine(), identifierToken.getText());
+		}
+
+		// Verifica se a variável foi declarada previamente.
+		newVar(identifierToken);
+
+		return Type.NO_TYPE;
+	}
+
+	/*
+	 *
+	 * array_declaration e array_ags
+	 *
+	 */
+
+	// Visita a regra array_declaration: L_BRACKET DECIMAL_LIT R_BRACKET var_types
+	@Override
+	public Type visitArray_declaration(GoParser.Array_declarationContext ctx) {
+		// Define o tamanho do array.
+		lastDeclArgsSize = Integer.parseInt(ctx.DECIMAL_LIT().getText());
+
+		visit(ctx.var_types());
+
+		return Type.NO_TYPE;
+	}
+
+	// Visita a regra array_init: array_declaration L_CURLY expression_list? R_CURLY
+	@Override
+	public Type visitArray_init(GoParser.Array_initContext ctx) {
+		// Recursively Visita a regra for error checking
+		visit(ctx.array_declaration());
+
+		if (ctx.expression_list() != null) {
+			// Recursively Visita a regra for error checking
+			visit(ctx.expression_list());
+		}
+
+		return Type.NO_TYPE;
+	}
+
+	/*
+	 *
+	 * input e output
+	 *
+	 */
+
+	// Visita a regra input: INPUT L_PAREN AMPERSAND id R_PAREN
+	@Override
+	public Type visitInput(GoParser.InputContext ctx) {
+		// Visita a regra recursivamente para verificar erros
+		visit(ctx.id());
+
+		return Type.NO_TYPE;
+	}
+
+	// Visita a regra output: OUTPUT L_PAREN expression_list? R_PAREN
+	@Override
+	public Type visitOutput(GoParser.OutputContext ctx) {
+		visit(ctx.expression_list());
+
+		return Type.NO_TYPE;
+	}
+
+
+	/*
+	 *
+	 * Funções
+	 *
+	 */
+
+	// Visita a regra func_declaration: FUNC IDENTIFIER L_PAREN func_args? R_PAREN var_types? statement_section
+	@Override
+	public Type visitFunc_declaration(GoParser.Func_declarationContext ctx) {
+
+		if (ctx.var_types() != null) {
+			// Define lastDeclType
+			visit(ctx.var_types());
+		} else {
+			// Função não retorna nenhum tipo
+			lastDeclType = Type.NO_TYPE;
+		}
+
+		// Salva o tupo da função antes do lastDeclType se sobrescrito pela declaração dos args
+		lastDeclFuncType = lastDeclType;
+
+		if (ctx.func_args() != null) {
+			// Define lastDeclArgsSize
+			visit(ctx.func_args());
+		} else {
+			lastDeclArgsSize = 0;
+		}
+
+		// Verifica se a função foi previamente declarada
+		newFunc(ctx.IDENTIFIER().getSymbol(), lastDeclArgsSize);
+
+		visit(ctx.statement_section());
+
+		return Type.NO_TYPE;
+	}
+
+
+	// Visita a regra func_args: id var_types (COMMA id var_types)*
+	@Override
+	public Type visitFunc_args(GoParser.Func_argsContext ctx) {
+		lastDeclArgsSize = ctx.id().size();
+
+		// Adiciona cada elemento na tabela de variáveis
+		for (int i = 0; i < ctx.id().size(); i++) {
+
+			visit(ctx.var_types(i));
+			// Verifica se a variável foi previamente declarada
+			newVar(ctx.id(i).IDENTIFIER().getSymbol());
+
+			visit(ctx.id(i));
+		}
+
+		return Type.NO_TYPE;
+	}
+
+	// Visita a regra func_section: func_declaration* func_main func_declaration*
+	@Override
+	public Type visitFunc_section(GoParser.Func_sectionContext ctx) {
+		//  Verifica se tem outra declaração de função além da função main
+		if (ctx.func_declaration() != null) {
+			// Visita recursivamente as funções para checar erros
+			for (GoParser.Func_declarationContext funcDecl : ctx.func_declaration()) {
+				visit(funcDecl);
+			}
+		}
+
+		// Visita a função main para checar erros
+		visit(ctx.func_main());
+
+		return Type.NO_TYPE;
+	}
+
+
+
+	/*
+	 *
+	 * Statements
+	 *
+	 */
+
+	// Visita a regra statement_section: L_CURLY statement* return_statement?
+	// R_CURLY
+	@Override
+	public Type visitStatement_section(GoParser.Statement_sectionContext ctx) {
+		// Visita cada statement para checar erros
+		for (GoParser.StatementContext stmt : ctx.statement()) {
+			visit(stmt);
+		}
+
+		if (ctx.return_statement() != null) {
+			visit(ctx.return_statement());
+		}
+
+		return Type.NO_TYPE;
+	}
+	
+	
+
+	// Visita a regra return_statement: RETURN expression SEMI?
+	@Override
+	public Type visitReturn_statement(GoParser.Return_statementContext ctx) {
+		Type expressionType = Type.NO_TYPE;
+
+		if (ctx.expression() != null) {
+			expressionType = visit(ctx.expression());
+		}
+
+		checkFuncReturnType(ctx.RETURN().getSymbol().getLine(), expressionType);
+
+		return Type.NO_TYPE;
+	}
+
+	// Visita a regra if_statement: IF expression statement_section (ELSE statement_section)?
+	@Override
+	public Type visitIf_statement(GoParser.If_statementContext ctx) {
+		Type expressionType = visit(ctx.expression());
+
+		// Varifica a expressão para ver se ela é do tipo bool
+		checkBoolExpr(ctx.IF().getSymbol().getLine(), "if", expressionType);
+
+		// Visita recursivamente statment_section do bloco if para checar erros
+		visit(ctx.statement_section(0));
+
+		// Visita recursivamente statment_section do bloco else para checar erros
+		if (ctx.ELSE() != null) {
+			visit(ctx.statement_section(1));
+		}
+
+		return Type.NO_TYPE;
+	}
+
+	
 
 }
