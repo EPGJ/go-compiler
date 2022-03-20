da318c0ec74ccdb1f6bb5dc44cd2ee3667ef5581
Elias Junior <eliasepgj@gmail.com>
2022-03-20 15:26:06 -0300

    implementando algums metodos, falta substituir comentarios pelo codigo no buffer de maneira apropriada

--- src/code/LLVMGenerator.java
+++ src/code/LLVMGenerator.java
@@ -1,8 +1,6 @@
 package code;
 
 import java.util.Scanner;
-
-import java.util.HashSet;
 import java.util.Stack;
 
 import ast.AST;
@@ -23,7 +21,7 @@ public class LLVMGenerator extends ASTBaseVisitor<Void>{
     private static int br = 0;
     private final StrTable st;
 	private final VarTable vt;
-    private final Scanner in;
+    // private final Scanner in;
 
     static Stack<Integer> br_stack = new Stack<>();
 
@@ -106,10 +104,10 @@ public class LLVMGenerator extends ASTBaseVisitor<Void>{
                 buffer+= "%" + reg++ +" = alloca float, align 4";
 				break;
 			case BOOL_TYPE:
-                buffer+= "%" + reg++ +" = alloca i32, align 4";
+                // buffer+= "%" + reg++ +" = alloca bool, align 4";
 				break;
 			case STRING_TYPE:
-                buffer+= "%" + reg++ +" = alloca i32, align 4";
+                // buffer+= "%" + reg++ +" = alloca i32, align 4";
 				break;
 		    default:
 	            System.err.printf("Invalid input type: %s!\n", varType.toString());
@@ -125,97 +123,362 @@ public class LLVMGenerator extends ASTBaseVisitor<Void>{
 
     @Override
     protected Void visitOutput(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+        // Get the expression list node
+		AST expressionList = node.getChild(0);
+
+		// Iterates over the expression list's children to print them
+		for (AST expression : expressionList.getChildren()) {
+			int x = visit(expression);
+			
+			switch(expression.type) {
+				case INT_TYPE:  	buffer+=x;		break;
+				case FLOAT32_TYPE: 	buffer+=x;		break;
+				case BOOL_TYPE: 	buffer+=x;   	break;
+				case STRING_TYPE:  	buffer+=x;		break;
+				case NO_TYPE:
+				default:
+					System.err.printf("Invalid output type: %s!\n", expression.type.toString());
+					System.exit(1);
+			}
+		}
+
+		return null;
     }
+
+
 
     @Override
     protected Void visitEquals(AST node) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        AST l = node.getChild(0);
+		AST r = node.getChild(1);
 
-    @Override
-    protected Void visitNotEquals(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+		// Visits each expression and get their registers
+		visit(l);
+		visit(r);
+
+		// Emits the 'equals operation' for the corresponding type
+		switch (l.type) {
+			case INT_TYPE:			/*codigo llvm icmp */	break;
+			case FLOAT32_TYPE:		/*codigo llvm icmp */	break;
+			case BOOL_TYPE:			/*codigo llvm icmp */	break;
+			case STRING_TYPE:		/*codigo llvm icmp */	break;
+			default:
+				System.err.printf("Invalid type: %s!\n", l.type.toString());
+				System.exit(1);
+		}
+
+	    return null;
     }
 
     @Override
     protected Void visitLess(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+        AST l = node.getChild(0);
+		AST r = node.getChild(1);
+
+		visit(l);
+		visit(r);
+
+		switch (l.type) {
+			case INT_TYPE:			/*codigo llvm slt */	break;
+			case FLOAT32_TYPE:		/*codigo llvm slt */	break;
+			case BOOL_TYPE:			/*codigo llvm slt */	break;
+			case STRING_TYPE:		/*codigo llvm slt */	break;
+			default:
+				System.err.printf("Invalid type: %s!\n", l.type.toString());
+				System.exit(1);
+		}
+
+	    return null;
     }
 
     @Override
     protected Void visitLessOrEquals(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+        AST l = node.getChild(0);
+		AST r = node.getChild(1);
+
+		visit(l);
+		visit(r);
+
+		switch (l.type) {
+			case INT_TYPE:			/*codigo llvm sle */	break;
+			case FLOAT32_TYPE:		/*codigo llvm sle */	break;
+			case BOOL_TYPE:			/*codigo llvm sle */	break;
+			case STRING_TYPE:		/*codigo llvm sle */	break;
+			default:
+				System.err.printf("Invalid type: %s!\n", l.type.toString());
+				System.exit(1);
+		}
+
+	    return null;
     }
 
     @Override
     protected Void visitGreater(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+        AST l = node.getChild(0);
+		AST r = node.getChild(1);
+
+		visit(l);
+		visit(r);
+
+		switch (l.type) {
+			case INT_TYPE:			/*codigo llvm sgt */	break;
+			case FLOAT32_TYPE:		/*codigo llvm sgt */	break;
+			case BOOL_TYPE:			/*codigo llvm sgt */	break;
+			case STRING_TYPE:		/*codigo llvm sgt */	break;
+			default:
+				System.err.printf("Invalid type: %s!\n", l.type.toString());
+				System.exit(1);
+		}
+
+	    return null;
     }
 
     @Override
     protected Void visitGreaterOrEquals(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+        AST l = node.getChild(0);
+		AST r = node.getChild(1);
+
+		visit(l);
+		visit(r);
+
+		switch (l.type) {
+			case INT_TYPE:			/*codigo llvm sge */	break;
+			case FLOAT32_TYPE:		/*codigo llvm sge */	break;
+			case BOOL_TYPE:			/*codigo llvm sge */	break;
+			case STRING_TYPE:		/*codigo llvm sge */	break;
+			default:
+				System.err.printf("Invalid type: %s!\n", l.type.toString());
+				System.exit(1);
+		}
+
+	    return null;
     }
 
     @Override
+	protected Void visitNotEquals(AST node) {
+		AST l = node.getChild(0);
+		AST r = node.getChild(1);
+
+		visit(l);
+		visit(r);
+
+		switch (l.type) {
+			case INT_TYPE:			/*codigo llvm ne */	break;
+			case FLOAT32_TYPE:		/*codigo llvm ne */	break;
+			case BOOL_TYPE:			/*codigo llvm ne */	break;
+			case STRING_TYPE:		/*codigo llvm ne */	break;
+			default:
+				System.err.printf("Invalid type: %s!\n", l.type.toString());
+				System.exit(1);
+		}
+
+	    return null;
+	}
+
+	/*------------------------------------------------------------------------------*
+	 *	Arithmetic operations
+	 *------------------------------------------------------------------------------*/
+
+    @Override
     protected Void visitStar(AST node) {
-        // TODO Auto-generated method stub
+        AST lNode = node.getChild(0);
+		AST rNode = node.getChild(1);
+
+		visit(lNode);
+		visit(rNode);
+
+		// Emits the 'multiply' for the corresponding type
+	    if (node.type == Type.FLOAT32_TYPE) {
+	        /* codigo llvm 
+            %5 = load float, float* %4, align 4
+            %6 = load i32, i32* %2, align 4
+            %7 = sitofp i32 %6 to float
+            %8 = fmul float %5, %7
+            store float %8, float* %3, align 4 
+            */
+	    } else {
+	        /* codigo llvm 
+            %5 = load i32, i32* %4, align 4
+            %6 = load i32, i32* %2, align 4
+            %7 = mul nsw i32 %5, %6
+            store i32 %7, i32* %3, align 4
+            */
+	    }
+
         return null;
     }
 
     @Override
     protected Void visitDiv(AST node) {
-        // TODO Auto-generated method stub
+        AST lNode = node.getChild(0);
+		AST rNode = node.getChild(1);
+
+		visit(lNode);
+		visit(rNode);
+
+		// Emits the 'div' for the corresponding type
+	    if (node.type == Type.FLOAT32_TYPE) {
+	        /* codigo llvm 
+            %5 = load float, float* %4, align 4
+            %6 = load i32, i32* %2, align 4
+            %7 = sitofp i32 %6 to float
+            %8 = fdiv float %5, %7
+            store float %8, float* %3, align 4
+            */
+	    } else {
+	        /* codigo llvm 
+            %5 = load i32, i32* %4, align 4
+            %6 = load i32, i32* %2, align 4
+            %7 = sdiv i32 %5, %6
+            store i32 %7, i32* %3, align 4
+            */
+	    }
+
         return null;
     }
 
     @Override
     protected Void visitMod(AST node) {
-        // TODO Auto-generated method stub
+        AST lNode = node.getChild(0);
+		AST rNode = node.getChild(1);
+
+		visit(lNode);
+		visit(rNode);
+
+		// Emits the 'mod' for the corresponding type
+
+        /* codigo llvm 
+        %5 = load i32, i32* %4, align 4
+        %6 = load i32, i32* %2, align 4
+        %7 = srem i32 %5, %6
+        store i32 %7, i32* %3, align 4
+        */
+	    
         return null;
     }
 
     @Override
     protected Void visitPlus(AST node) {
-        // TODO Auto-generated method stub
+        AST lNode = node.getChild(0);
+		AST rNode = node.getChild(1);
+
+		visit(lNode);
+		visit(rNode);
+
+		// Emits the 'sum' for the corresponding type
+	    if (node.type == Type.FLOAT32_TYPE) {
+	        /* codigo llvm 
+            %5 = load float, float* %4, align 4
+            %6 = load float, float* %2, align 4
+            %7 = fadd float %5, %6
+            store float %7, float* %3, align 4
+            */
+	    } else {
+	        /* codigo llvm 
+            %5 = load i32, i32* %4, align 4
+            %6 = load i32, i32* %2, align 4
+            %7 = add nsw i32 %5, %6
+            store i32 %7, i32* %3, align 4
+            */
+	    }
         return null;
     }
 
     @Override
     protected Void visitMinus(AST node) {
-        // TODO Auto-generated method stub
+        AST lNode = node.getChild(0);
+		AST rNode = node.getChild(1);
+
+		visit(lNode);
+		visit(rNode);
+
+		// Emits the 'minus' for the corresponding type
+	    if (node.type == Type.FLOAT32_TYPE) {
+	        /* codigo llvm 
+            %5 = load float, float* %4, align 4
+            %6 = load float, float* %2, align 4
+            %7 = fsub float %5, %6
+            store float %7, float* %3, align 4
+            */
+	    } else {
+	        /* codigo llvm 
+            %5 = load i32, i32* %4, align 4
+            %6 = load i32, i32* %2, align 4
+            %7 = sub nsw i32 %5, %6
+            store i32 %7, i32* %3, align 4
+            */
+	    }
         return null;
     }
 
+    
+
     @Override
     protected Void visitStatementSection(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+        for (AST child : node.getChildren()) {
+			visit(child);
+		}
+		return null; 
     }
 
     @Override
     protected Void visitReturn(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+        // Checks if there is an expression node child
+		if (node.getChildren().size() == 1) {
+			// Visits the expression
+			visit(node.getChild(0));
+		}
+
+		return null; 
     }
 
     @Override
     protected Void visitVarDecl(AST node) {
-        // TODO Auto-generated method stub
-        return null;
+        // Checks if the variable was assigned a value at declaration
+		if(node.getChildren().size() > 0) {
+			// Visits the expression to push its value to the stack
+			visit(node.getChild(0));
+	
+			// Get the var index and type 
+			int varIdx = node.intData;
+			Type varType = vt.getType(varIdx);
+	
+			// Emits the 'store word' for the corresponding type
+			if (varType == Type.FLOAT32_TYPE) {
+				/*  llvm code
+                %1 = alloca i32, align 4
+                %2 = alloca float, align 4
+                store i32 0, i32* %1, align 4
+                ret i32 0
+                */
+			} else {
+				/* llvm code
+                %1 = alloca i32, align 4
+                %2 = alloca i32, align 4
+                store i32 0, i32* %1, align 4
+                ret i32 0
+                */
+			}
+		}
+
+		return null;
     }
 
     @Override
     protected Void visitAssign(AST node) {
-        // TODO Auto-generated method stub
+        // Visits the expression to push its value to the stack
+		visit(node.getChild(1));
+
+		// Get the var index and type 
+		int varIdx = node.getChild(0).intData;
+		Type varType = vt.getType(varIdx);
+
+	    if (varType == Type.FLOAT32_TYPE) {
+	        emit(STWf, addr, x);
+	    } else {
+	        emit(STWi, addr, x);
+	    }
+
         return null;
     }
 
